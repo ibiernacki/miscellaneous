c402e51a6e1d4802098679fecfdcc60ed61d4e9b
Ignacy Biernacki <ignac.biernacki@gmail.com>
2022-06-06 18:30:57 +0200

    Update provision.ps1

--- profile/pwsh/provision.ps1
+++ profile/pwsh/provision.ps1
@@ -11,7 +11,7 @@ if (!$isAdmin) {
     exit;
 }
 
-if(! (Start-Process where.exe -ArgumentList "/Q", "choco" -PassThru -Wait -WindowStyle Hidden).ExitCode -eq 0) {
+if( (Start-Process where.exe -ArgumentList "/Q", "choco" -PassThru -Wait -WindowStyle Hidden).ExitCode -eq 1) {
     Write-Warning "choco not installed. Go to chocolatey website and install: https://chocolatey.org/"
     exit;
 }
